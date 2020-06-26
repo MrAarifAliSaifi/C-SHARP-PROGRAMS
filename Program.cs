b2e35402b732323a43e8ad4144cb119650ebed0a
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2020-06-26 12:46:05 +0530

    factorial of a given number

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int factorial = 1;
+
+            Console.WriteLine("PLEASE ENTER THE NO TO FIND OUT THE FACTORIAL:");  //taking input from the usr
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i=n;i>0;i--)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine("THE FACTORIAL OF THE GIVEN NUMBER IS:"+factorial);
+            Console.ReadLine();
+
+        }
+    }
+}
